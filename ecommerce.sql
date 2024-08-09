8020736651997bc8db4e812f39068e1c5f012ddf
Arthur Alves Silva <104282615+ArthurrAlv@users.noreply.github.com>
2024-08-09 12:06:59 -0300

    DATA BASE UP

---
+++ ecommerce.sql
@@ -0,0 +1,189 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 05/08/2024 às 17:00
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `ecommerce`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nome`, `email`, `senha`) VALUES
+(1, 'Cliente 1', 'cliente1@email.com', '123'),
+(2, 'Cliente 2', 'cliente2@email.com', '123'),
+(3, 'Arthur', 'a@gmail.com', '$2y$10$zhUcV2O5EmBrywWXoq6ruO01bQ9I38BrQIvFLJy2lNfYxSgcsIt8S'),
+(4, 'Arthur', '1@gmail.com', '$2b$10$FW7xLZWqzNocaTPFQcVM.u9YL4OJsltIpbCqrDsfUYBslBArZK6Ee');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `descricao` text DEFAULT NULL,
+  `preco` decimal(10,2) DEFAULT NULL,
+  `vendedor_id` int(11) NOT NULL,
+  `estoque` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `nome`, `descricao`, `preco`, `vendedor_id`, `estoque`) VALUES
+(1, 'Produto 1', 'Descrição do Produto 1', 100.00, 1, 0),
+(2, 'Produto 2', 'Descrição do Produto 2', 150.00, 1, 0),
+(3, 'Produto 3', 'Descrição do Produto 3', 80.00, 2, 0),
+(4, 'Produto 4', 'Descrição do Produto 4', 200.00, 2, 0),
+(18, 'Arthur', 'Xmais', 0.00, 3, 0),
+(20, 'Arthur Alves Silva', 'testee', 11.11, 3, 0),
+(21, 'aa', 'x', 0.00, 3, 0),
+(22, 'Arthur', 'Xmais', 1.00, 3, 0),
+(25, 'favorito', 'melhor', 1.00, 0, 0),
+(26, '1111111111', '111111111111', 11111111.00, 0, 0),
+(27, '2222222', '222222222', 2222222.00, 0, 1),
+(28, '333333333', '33333333333', 33333333.00, 4, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `vendedores`
+--
+
+CREATE TABLE `vendedores` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `vendedores`
+--
+
+INSERT INTO `vendedores` (`id`, `nome`, `email`, `senha`) VALUES
+(1, 'Vendedor 1', 'vendedor1@email.com', '123'),
+(2, 'Vendedor 2', 'vendedor2@email.com', '123'),
+(3, 'Arthur A', 'a@email.com', '$2y$10$en0uEGXHexDZRZgBEqQQhONjgL12u.3ldrIy7vgOVclKq6sstGXe.'),
+(4, 'vendedor1', '1@gmail.com', '$2b$10$j7spTy1Thg6Yok.hrbKU7.fEf6NiCnLLJataT.x.rZ55U6fXWCnXu');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Índices de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Índices de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vendedor_id` (`vendedor_id`);
+
+--
+-- Índices de tabela `vendedores`
+--
+ALTER TABLE `vendedores`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT de tabela `vendedores`
+--
+ALTER TABLE `vendedores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `produtos`
+--
+ALTER TABLE `produtos`
+  ADD CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`vendedor_id`) REFERENCES `vendedores` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
