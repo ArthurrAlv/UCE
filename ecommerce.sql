e13350ff0762666cac510e9bf0d120ec2c990667
Arthur Alves Silva <104282615+ArthurrAlv@users.noreply.github.com>
2024-08-15 11:22:22 -0300

    Update ecommerce.sql
    banco de dados

--- ecommerce.sql
+++ ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12/08/2024 às 17:05
+-- Tempo de geração: 15/08/2024 às 16:21
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -24,147 +24,57 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `admins`
+-- Estrutura para tabela `carrinhos`
 --
 
-CREATE TABLE `admins` (
+CREATE TABLE `carrinhos` (
   `id` int(11) NOT NULL,
-  `nome` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `senha` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `clientes`
---
-
-CREATE TABLE `clientes` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `senha` varchar(255) NOT NULL
+  `cliente_id` int(11) NOT NULL,
+  `produto_id` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL DEFAULT 1,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `clientes`
+-- Despejando dados para a tabela `carrinhos`
 --
 
-INSERT INTO `clientes` (`id`, `nome`, `email`, `senha`) VALUES
-(1, 'cliente', 'cliente@gmail.com', '$2b$10$qBNYTIvO1I5Rt1eXOg5lpeftNoA..jaCaQsubIksQGxlp8NqxoXRm');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `produtos`
---
-
-CREATE TABLE `produtos` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `descricao` text DEFAULT NULL,
-  `preco` decimal(10,2) NOT NULL,
-  `vendedor_id` int(11) NOT NULL,
-  `estoque` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `produtos`
---
-
-INSERT INTO `produtos` (`id`, `nome`, `descricao`, `preco`, `vendedor_id`, `estoque`) VALUES
-(1, 'produto 1', 'descrição do produto 1', 1.99, 1, 2);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `vendedores`
---
-
-CREATE TABLE `vendedores` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `senha` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `vendedores`
---
-
-INSERT INTO `vendedores` (`id`, `nome`, `email`, `senha`) VALUES
-(1, 'vendedor', 'vendedor@gmail.com', '$2b$10$DoJmtxU8i.z/ZKpnc9ehHeQ7AdHwzPsYw97Tkp0n88wD71kfp5OWy');
+INSERT INTO `carrinhos` (`id`, `cliente_id`, `produto_id`, `quantidade`, `createdAt`, `updatedAt`) VALUES
+(9, 1, 1, 4, '2024-08-15 01:36:34', '2024-08-15 11:42:58');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `admins`
+-- Índices de tabela `carrinhos`
 --
-ALTER TABLE `admins`
+ALTER TABLE `carrinhos`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Índices de tabela `clientes`
---
-ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Índices de tabela `produtos`
---
-ALTER TABLE `produtos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `vendedor_id` (`vendedor_id`);
-
---
--- Índices de tabela `vendedores`
---
-ALTER TABLE `vendedores`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD KEY `cliente_id` (`cliente_id`),
+  ADD KEY `produto_id` (`produto_id`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT de tabela `admins`
+-- AUTO_INCREMENT de tabela `carrinhos`
 --
-ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `clientes`
---
-ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de tabela `produtos`
---
-ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de tabela `vendedores`
---
-ALTER TABLE `vendedores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `carrinhos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restrições para tabelas despejadas
 --
 
 --
--- Restrições para tabelas `produtos`
+-- Restrições para tabelas `carrinhos`
 --
-ALTER TABLE `produtos`
-  ADD CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`vendedor_id`) REFERENCES `vendedores` (`id`);
+ALTER TABLE `carrinhos`
+  ADD CONSTRAINT `carrinhos_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `carrinhos_ibfk_2` FOREIGN KEY (`produto_id`) REFERENCES `produtos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
